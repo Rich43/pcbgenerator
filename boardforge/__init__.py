7a1850ae6c1946a133c641cdd6c6adce41c4ebb8
Richard Ward <richard.ward637@outlook.com>
2025-07-17 09:25:03 +0100

    Add higher level API with routing helpers

--- boardforge/__init__.py
+++ boardforge/__init__.py
@@ -4,6 +4,7 @@ from .Component import Component
 from .Via import Via
 from .Graphic import Graphic
 from .Board import Board
+from .Zone import Zone
 from .circuits import (
     create_voltage_divider,
     create_led_indicator,
@@ -14,3 +15,44 @@ from .circuits import (
 # Default symbolic layer names
 TOP_SILK = "GTO"
 BOTTOM_SILK = "GBO"
+
+PCB = Board
+
+from enum import Enum
+
+
+class Layer(str, Enum):
+    TOP_COPPER = "GTL"
+    BOTTOM_COPPER = "GBL"
+    TOP_SILK = TOP_SILK
+    BOTTOM_SILK = BOTTOM_SILK
+
+
+class Footprint(Enum):
+    ESP32_WROOM = "ESP32_WROOM"
+    SOP16 = "SOP16"
+    SOT223 = "SOT223"
+    USB_C_CUTOUT = "USB_C_CUTOUT"
+    C0603 = "C0603"
+    TACTILE_SWITCH = "TACTILE_SWITCH"
+    HEADER_1x5 = "HEADER_1x5"
+
+
+__all__ = [
+    "Board",
+    "PCB",
+    "Component",
+    "Pin",
+    "Pad",
+    "Via",
+    "Zone",
+    "Graphic",
+    "Layer",
+    "Footprint",
+    "create_voltage_divider",
+    "create_led_indicator",
+    "create_rc_lowpass",
+    "create_bent_trace",
+    "TOP_SILK",
+    "BOTTOM_SILK",
+]
